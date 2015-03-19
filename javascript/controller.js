de49a16e2c396e41f97f326691112b997b2c0014
Nick Smit <nick.smit@gmail.com>
2015-03-19 22:06:56 +0100

    removed unecessary package

--- javascript/controller.js
+++ javascript/controller.js
@@ -4,10 +4,9 @@
   /* Controllers */
 
   var jobsApp = angular.module('angularApp', []);
-  // var api = 'http://tinder-api.herokuapp.com/people';
   var api = 'https://hiring-proto.herokuapp.com/api/jobs';
 
-  jobsApp.controller('jobsListCtrl', function ($scope, $http) {
+  jobsApp.controller('JobsListCtrl', function ($scope, $http) {
     $http.get(api).success(function(data) {
 
       // Data contains ajax response from API
